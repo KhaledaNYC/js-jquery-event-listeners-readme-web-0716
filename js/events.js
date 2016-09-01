a1fc769cd1c7fc2e2ed76694e322ab93b073b4d3
KhaledaNYC <khaleda.khan87@gmail.com>
2016-08-31 20:05:39 -0400

    event listern

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt(){
+  $('p').click(alert('Hey!'));
+}
 
-// call functions here
+
+function frameIt() {
+  $('img').on('load', function() {
+      $(this).addClass('tasty')
+    });
+  }
+
+
+function pressIt() {
+  $('#typing').on('keydown',function(key){
+    if (key.which == 71) {
+      alert("You pressed letter g.");
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit',function () {
+    alert('Your form is going to be submitted now.');
+  });
+
+}
+
+$(document).ready(function(){
+  getIt();
+  // call functions here
 
 });
